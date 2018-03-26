03f3331e30277673ed0d8c23af43286207cd8ad9
Eden Mesfun <emesfun@gmail.com>
2018-03-26 16:42:34 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -15,7 +15,6 @@ class App < Sinatra::Base
   post '/checkout' do
     session[:item] = params[:item]
     @session = session
-    session[:message] = "Shopping cart contents include #{params[:item]}."
     redirect to ('/index')
   end
 
